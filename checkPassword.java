ad8fdb3552c129d6261bb65569587a627de1273e
James <jamesbasanmhs@gmail.com>
2021-02-06 17:42:29 -0800

    cleaned up the indentations

--- checkPassword.java
+++ checkPassword.java
@@ -20,91 +20,91 @@ public class checkPassword{
         while (playing) {
 
             // ask user if they want input to be hidden in the Terminal
-	        System.out.println("Would you like your input to be hidden? (y/n):");
-	        while (!hidden.matches("(?i)y|n")) {
-	            hidden = in.nextLine();
-	                
-	            if (!hidden.matches("(?i)y|n")) {
-	                System.out.println("Invalid input. (y/n):");
-	            }
-	        }
+            System.out.println("Would you like your input to be hidden? (y/n):");
+            while (!hidden.matches("(?i)y|n")) {
+                hidden = in.nextLine();
+                    
+                if (!hidden.matches("(?i)y|n")) {
+                    System.out.println("Invalid input. (y/n):");
+                }
+            }
 
-	        // receive hidden password input
-	        if (hidden.matches("(?i)y")) {
-	            Console cons;
-	            char[] passwd;
-	                
-	            if ((cons = System.console()) != null &&
-	                (passwd = cons.readPassword("%s", "Enter your password:\n")) != null) {
-	                    userPassword = new String(passwd);
-	                    java.util.Arrays.fill(passwd, ' ');
-	            }
-	            else {
-	                    System.out.println("Error: Could not set up system console for user input.");
-	                    return;
-	            }
-	        }
+            // receive hidden password input
+            if (hidden.matches("(?i)y")) {
+                Console cons;
+                char[] passwd;
+                    
+                if ((cons = System.console()) != null &&
+                    (passwd = cons.readPassword("%s", "Enter your password:\n")) != null) {
+                    userPassword = new String(passwd);
+                    java.util.Arrays.fill(passwd, ' ');
+                }
+                else {
+                    System.out.println("Error: Could not set up system console for user input.");
+                    return;
+                }
+            }
             // receive visible password input
-	        else {
-	            System.out.println("\nEnter your password:");
-	            userPassword = in.nextLine();
-	        }
+            else {
+                System.out.println("\nEnter your password:");
+                userPassword = in.nextLine();
+            }
 
 
-	        File file = new File("rockyou.txt");
-	        if (file.exists()){
+            File file = new File("rockyou.txt");
+            if (file.exists()){
 
-	            if (file.isFile() && file.canRead()){
-	                
-	                BufferedReader br = null;
+                if (file.isFile() && file.canRead()){
+                    
+                    BufferedReader br = null;
 
-	                try {
-	                    // BufferedReader reads from the password file
-	                    br = new BufferedReader(new FileReader(file));
-	                    String filePassword;
+                    try {
+                        // BufferedReader reads from the password file
+                        br = new BufferedReader(new FileReader(file));
+                        String filePassword;
 
-	                    while ((filePassword = br.readLine()) != null){
-	                        //System.out.println(filePassword);
+                        while ((filePassword = br.readLine()) != null){
+                            //System.out.println(filePassword);
 
                             // password was found in the password file
-	                        if (userPassword.equals(filePassword)){
+                            if (userPassword.equals(filePassword)){
                                 matchedPassword = true;
-	                            if (hidden.matches("(?i)y")) {
-	                                System.out.println("\nOh no! Your password is common!");
-	                            }
-	                            else {
-	                                System.out.format("\nOh no! Your password (%s) is common!\n", userPassword);
-	                            }
-	                            break;
-	                        }
-	                    }
-	                }
+                                if (hidden.matches("(?i)y")) {
+                                    System.out.println("\nOh no! Your password is common!");
+                                }
+                                else {
+                                    System.out.format("\nOh no! Your password (%s) is common!\n", userPassword);
+                                }
+                                break;
+                            }
+                        }
+                    }
 
-	                catch (IOException exception) {
-	                    exception.printStackTrace();
-	                }
+                    catch (IOException exception) {
+                        exception.printStackTrace();
+                    }
 
-	                finally {
-	                    try {
-	                        if (br != null) {
-	                            br.close();
-	                        }
-	                    }
-	                    catch (IOException exception) {
-	                        exception.printStackTrace();
-	                    }
-	                }
+                    finally {
+                        try {
+                            if (br != null) {
+                                br.close();
+                            }
+                        }
+                        catch (IOException exception) {
+                            exception.printStackTrace();
+                        }
+                    }
 
-	            }
-	        }
+                }
+            }
 
-	        if (!matchedPassword) System.out.println("\nCongrats! Your password is not common!");
+            if (!matchedPassword) System.out.println("\nCongrats! Your password is not common!");
 
             // ask if the user wants to run the program again
             System.out.println("\nWould you like to try another input? (y) or type anything else to quit:");
-	        if (!in.nextLine().matches("(?i)y")) {
-	            playing = false;
-	        }
+            if (!in.nextLine().matches("(?i)y")) {
+                playing = false;
+            }
 
             System.out.println();
             matchedPassword = false;
